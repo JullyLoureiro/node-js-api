a3e506b9d2775af0cf3dfe9c3a68d670bc124c83
JullyLoureiro <juliana.ferreira@soulasalle.com.br>
2021-04-07 10:40:57 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//Node não é compilado, é interpretado
+//Servidor web: fica ouvindo a porta XPTO
+
+//Instalar modulos: http, express, debug
+
+//Força o javascript a ser mais criterioso com a sintaxe 
+'use strict'
+
+console.log('Testando...');
